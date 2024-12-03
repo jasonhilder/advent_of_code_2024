c1c66516f4785c6f7df1b3b188627f37bdf66d0e
Jason Hilder <jhilder95@gmail.com>
2024-12-03 07:56:55 +0200

    Added template for new days.

---
+++ day_00_template/src/main.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+
+//#define HELPFUL_IMPLEMENTATION
+//#include "../libs/helpful_c/helpful.h"
+
+//#define STB_DS_IMPLEMENTATION
+//#include "../libs/stb_ds.h"
+
+int main(void)
+{
+    return 0;
+}
